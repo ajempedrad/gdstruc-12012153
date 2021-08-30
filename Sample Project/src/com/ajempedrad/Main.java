7fc8f684d1264650a58fca42dcebe62a55daf4f8
AJ-PC\AJ <aaliyahjean.empedrad@benilde.edu.ph>
2021-08-30 17:37:44 +0800

    Added Sample Project

---
+++ Sample Project/src/com/ajempedrad/Main.java
@@ -0,0 +1,18 @@
+package com.ajempedrad;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
